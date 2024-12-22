e8400e1a4053dc03ddf5f69d801fd87f4c23b949
David Xavier <xdplansprojetos@gmail.com>
2024-12-22 04:01:37 -0300

    keyword

--- src/App.js
+++ src/App.js
@@ -7,17 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eletricista 24 Horas
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        
+             </header>
     </div>
   );
 }
